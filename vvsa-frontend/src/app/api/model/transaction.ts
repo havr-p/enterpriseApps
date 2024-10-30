8ef11b5aca98c2c45bf563e935a29293c545ca69
havriilp <havriil.pietukhin@consultis.sk>
2024-10-30 17:03:35 +0100

    add files

---
+++ vvsa-frontend/src/app/api/model/transaction.ts
@@ -0,0 +1,9 @@
+export interface ITransaction {
+  transactionId: number;
+  fullName: string;
+  transactionType: number;
+  accountNumber: string;
+  bankCode: string;
+  issueDate: Date;
+  amount: number;
+}
