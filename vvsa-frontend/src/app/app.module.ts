732d398441aba004d50eca9b210f4e67f905f18f
havriilp <5364429@gmail.com>
2024-11-06 16:01:44 +0100

    basic transactions info

--- vvsa-frontend/src/app/app.module.ts
+++ vvsa-frontend/src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {MatIcon, MatIconModule} from "@angular/material/icon";
-import {MatButtonModule, MatIconButton} from "@angular/material/button";
-import {MatMenu, MatMenuItem, MatMenuModule} from "@angular/material/menu";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatMenuModule} from "@angular/material/menu";
 import {MatToolbar} from "@angular/material/toolbar";
-import { TransactionTypePipe } from './utils/pipes/transaction-type.pipe';
 
 
 @NgModule({
@@ -27,4 +26,5 @@ import { TransactionTypePipe } from './utils/pipes/transaction-type.pipe';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
